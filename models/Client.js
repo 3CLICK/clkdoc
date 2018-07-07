b1b93d1c1c24a68770d7a8061a191a0e9b25185c
Nelson Diaz <abnerdc86@gmail.com>
2018-07-07 02:53:55 -0400

    Updating models and mongoose version

--- models/Client.js
+++ models/Client.js
@@ -2,18 +2,47 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const clientProfile = new Schema({
-  _clientID: { type: Schema.Types.ObjectId, ref: "User" },
-  ssn: { type: Number, required: true, unique: true },
-  address: { type: String, required: true, unique: true },
-  city: { type: String, required: true, },
-  state: { type: String, required: true },
-  zipCode: { type: Number, required: true },
-  phoneNumber: { type: Number, required: true, },
-  emergencyContact: {
-    name: { type: String, required: true },
-    phone: { type: Number, required: true }
+  _id: Schema.Types.ObjectId,
+  ssn: {
+    type: Number,
+    required: true,
+    unique: true
   },
-  insurance: { type: String, required: true },
+  address: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zipCode: {
+    type: Number,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  emergencyContact: {
+    name: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: Number,
+      required: true
+    }
+  },
+  insurance: {
+    type: String,
+    required: true
+  },
   allergies: [String],
   reason_for_visit: String
 });
--- models/Doctor.js
+++ models/Doctor.js
@@ -2,13 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const doctorProfileShema = new Schema({
-  _docID: { type: Schema.Types.ObjectId, ref: "User" },
-  specialty: { type: String, required: true },
-  address: { type: String, required: true, unique: true },
-  city: { type: String, required: true, },
-  state: { type: String, required: true },
-  zipCode: { type: Number, required: true },
-  phoneNumber: { type: String, required: true, },
+  _id: Schema.Types.ObjectId,
+  specialty: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: true 
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zipCode: {
+    type: Number,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
   insurance: [String],
   languages: [String],
 });
