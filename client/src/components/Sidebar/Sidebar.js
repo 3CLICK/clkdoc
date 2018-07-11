bd0438b5e47ec288ecbe97b47852584cf8970639
VirtualVasquez <mxv647@med.miami.edu>
2018-07-10 20:45:38 -0400

    Reworking/connecting components, mainpage almost done.

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Sidebar.css"
 
-//NEED THESE FUNCTIONS IN SOMEHOW ON THE PAGE
-    // function w3_open() {
-    //     document.getElementById("mySidebar").style.display = "block";
-    // }
+class Sidebar extends Component {
 
-    // function w3_close() {
-    //     document.getElementById("mySidebar").style.display = "none";
-    // }
-//THE SPAN TAG NEEDS THE FOLLOWING PASSED THROUGH
+    openNav () {
+        document.getElementById("mySidenav").style.width = "250px";
+    };
 
-const Sidebar = props => (
+    closeNav () {
+        document.getElementById("mySidenav").style.width = "0px";
+    };
 
 
-  <a class="align-left">
-      <span class="glyphicon glyphicon-th pull-left" aria-hidden="true" {...props}></span>
-  </a>
+    render() {
+      return (
+      <div>
+        <a className="align-left">
+          <span className="glyphicon glyphicon-th pull-left" aria-hidden="true" onClick={this.openNav}  />
+        </a>
+        <div id="mySidenav" class="sidenav">
+          <a href="javascript:void(0)" class="closebtn" onClick={this.closeNav}>&times;</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+      )
+    };
 
-	{/*sidebar*/}
-    <div class="w3-sidebar w3-bar-block w3-border-right" style="display:none;top:0;left:0;position:absolute;" id="mySidebar">
-      <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
-      <a href="#" class="w3-bar-item w3-button">Home</a>
-      <a href="#" class="w3-bar-item w3-button">About</a>
-      <a href="#" class="w3-bar-item w3-button">Contact</a>
-    </div>
-
-);
+};
 
 export default Sidebar;
--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,37 +1,31 @@
-import React, { Components } from "react";
+import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import MainSearch from "../../components/MainSearch";
 import Footer from "../../components/Footer";
 import Sidebar from "../../components/Sidebar";
-import API from "../../utils/API";
+import DocDropdown from "../../components/DocDropdown";
+import PatientDropdown from "../../components/PatientDropdown";
+import GuestDropdown from "../../components/GuestDropdown";
 
 
 class MainPage extends Component {
-  docSearch = {
-    zip: "",
-    specialty: "",
-    synopsis: "",
-  }
-
-  w3_open = event => {
-    document.getElementById("mySidebar").style.display = "block";
-  };
-  
-  w3_close = event => {
-    document.getElementById("mySidebar").style.display = "none";
-  };
 
   render() {
     return (
-      <Nav>
-        <Sidebar onClick={this.w3_open}/>
-      </Nav>
       <div class="container">
-        <MainSearch/>
+        <Nav title="ClkDoc" sidebar={<Sidebar/>}/>
+
+            
+            
+        <MainSearch />
+        <Footer/>
       </div>
-      <Footer/>
     );
-  }
+  };
 
 
-}
+};
+
+export default MainPage;
+
+
