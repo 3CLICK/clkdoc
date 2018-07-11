dac717294eff2cabcb7c5ca93d1944dd2c21e094
VirtualVasquez <mxv647@med.miami.edu>
2018-07-11 13:41:29 -0400

    Pushing Components for sharing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainPage from "./pages/MainPage/MainPage.js";
+import DocResults from "./pages/DocResults/DocResults.js";
 
-const App = () => ( <MainPage /> );
+const App = () => ( <DocResults /> );
 
 export default App;
