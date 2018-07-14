d277e7754dfeab7e0e18f4634b5186d5821cbc11
VirtualVasquez <mxv647@med.miami.edu>
2018-07-13 21:03:38 -0400

    Updating CSS, forms betters aligned and adaptable

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -12,7 +12,8 @@ class Results extends Component {
       render() {
       return (
         <div class="container">
-          <form className="form form-inline col-xs-12 pull-right" id="resultRow" >
+
+          <form className="form form-inline col-xs-12 pull-right" id="resultRow">
                 <div className="form-group">
                 <h3 class="pull-left" id="resultLabel">RESULTS FOR:</h3>
                 </div>
@@ -52,81 +53,54 @@ class Results extends Component {
                   <button onClick={this.handleFormSubmit} className="btn btn-default form-inline ">Submit</button>
                   <button onClick="" className="btn btn-default form-inline ">Reset</button>
           </form>
-     
+    
 
+          <form className="form-inline pull-right" id="filterRow">
 
-            <div class="col-xs-3" id="filterLabel">
-              <h2>Filter By:</h2>
+            <div className="form-group">
+              <h3 class="pull-left" id="filterLabel">FILTER BY:</h3>
             </div>
 
-            <div class="col-xs-9">
-              <form className="form form-inline col-xs-12 pull-right">
-                <div class="form-group col-xs-3">
-                  <label htmlFor="rating">Rating:</label>   
-                  <select name="Rating">
-                    <option value={1}>*</option>
-                    <option value={2}>**</option>
-                    <option value={3}>***</option>
-                    <option value={4}>****</option>
-                    <option value={5}>*****</option>
-                  </select>
-                </div>
-                <div class="form-group col-xs-3">
-                  <label htmlFor="distance">Distance:</label>   
-                  <select name="distance">
-                    <option value={5}>5 Miles</option>
-                    <option value={10}>10 Miles</option>
-                    <option value={15}>15 Miles</option>
-                    <option value={20}>20 Miles</option>
-                    <option value={25}>25 Miles</option>
-                  </select>
-                </div>
-                <div class="form-group col-xs-3">
-                  <label htmlFor="docName">Doctor's Name:</label>
-                  <input type="docName" class="form-control" id="docName" />
-                </div>
-                <div class="form-group col-xs-offset-1 col-xs-1">
-                  <label htmlFor="reset">Reset</label>
-                  <button type="reset" class="btn btn-default">Reset</button>
-                </div> 
-              </form>
+            <div class="form-group">
+              <label for="Rating">Rating:</label>   
+                <select nameClass="form-control">
+                  <option value={1}>*</option>
+                  <option value={2}>**</option>
+                  <option value={3}>***</option>
+                  <option value={4}>****</option>
+                  <option value={5}>*****</option>
+                </select>
             </div>
 
-
-          <div class="row">
-            <div class="container text-center">
-              <h2>Here Are The Doctors We Found</h2>
-            </div>  
-{/*            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
-              <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to={0} class="active" />
-                <li data-target="#myCarousel" data-slide-to={1} />
-                <li data-target="#myCarousel" data-slide-to={2} />
-              </ol>
-              <div class="carousel-inner jumbotron" width={460} height={345}>
-                <div class="item active">
-                  <p>ONE AND ONLY ONE div needs to have the class "item active". ALL slides need to have the class "item" AND be nested inside the div with class "carousel slide". Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <div class="item">
-                  Dummy text
-                </div>
-              </div>
-              <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" />
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" />
-                <span class="sr-only">Next</span>
-              </a>
-            </div>*/}
-            <div class="row" id="found">
-              <div className="container">
-                <p className="text-center">Need to map out the DocCards here</p>
-              </div>
+            <div class="form-group">
+              <label>Distance:</label>   
+                <select nameClass="form-control">
+                  <option value={5}>5 Miles</option>
+                  <option value={10}>10 Miles</option>
+                  <option value={15}>15 Miles</option>
+                  <option value={20}>20 Miles</option>
+                  <option value={25}>25 Miles</option>
+                </select>
             </div>
-          </div>
+
+            <div className="form-group">
+              <label>Doctor's Name:</label>
+                <input 
+                  type="docName" 
+                  className="form-control" 
+                  id="docName" />
+            </div>
+
+            <div class="form-group">
+                <button type="reset" class="btn btn-default pull-right">Reset</button>
+            </div>
+
+          </form>
+
         </div>
+
+
+
         );
       };
 
