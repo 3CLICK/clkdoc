0b778ffbbb574456ba96708b8d8d7c09349e4d1e
VirtualVasquez <mxv647@med.miami.edu>
2018-07-13 23:09:49 -0400

    Fix CSS for DocCard, Footer, Results, and background

--- client/src/components/DocCard/DocCard.js
+++ client/src/components/DocCard/DocCard.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DocCard.css";
 
-const DocCard = props =>(
+class DocCard extends Component {
 
-<div class="card col-xs-3">
-  <img class="card-img-top col-xs-8 col-xs-offset-2" src="https://images8.alphacoders.com/712/712496.jpg" alt="image" style="width:150px;height: 150px;"/>
-  <div class="card-body text-center col-xs-12">
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <h5 class="card-title">Robert Ayala</h5>
-    <h6 class="card-text">Cardiologist</h6>
-    <h6 class="card-text">555 SW 40 Ave. 33197</h6>
-    <a href="#" class="btn btn-primary">Select</a>
-      {/*Need if/else function. Sign-in modal, view doc profile otherwise*/}
-  </div>
-</div>
-);
+  render() {
+    return (
+      <div className="card" style={{width: 150, height: 300}}>
+        <img className="card-img-top" src="https://images8.alphacoders.com/712/712496.jpg" alt="Card image cap" style={{width: 148, height: 150}} />
+        <div className="card-body text-center col-xs-12">
+          <span className="glyphicon glyphicon-star" />
+          <span className="glyphicon glyphicon-star" />
+          <span className="glyphicon glyphicon-star" />
+          <span className="glyphicon glyphicon-star" />
+          <span className="glyphicon glyphicon-star" />
+          <h5 className="card-title">Robert Ayala</h5>
+          <h6 className="card-text">Cardiologist</h6>
+          <h6 className="card-text">555 SW 40 Ave. 33197</h6>
+          <a href="#" className="btn btn-primary">Select</a>
+          {/*Need if/else function. Sign-in modal, view doc profile otherwise*/}
+        </div>
+      </div>
+    );
+  };
+};
 
 
 
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = props => (
 
 <footer>
         <ul className="nav nav-pills" display>
-          <li role="presentation" className="disabled"><a href="#">Terms of Service</a></li>
-          <li role="presentation" className="disabled"><a href="#">Privacy Policy</a></li>
-          <li role="presentation" className="disabled"><a href="#">Site Map</a></li>
-          <li role="presentation" className="disabled"><a href="#"><strong className="pull-right">clkDoc © 2018</strong></a></li>
-          <li className="pull-right"><a className="fa fa-github" style={{color: 'white'}} /></li>
-          <li className="pull-right"><a className="fa fa-twitter" style={{color: 'white'}} /></li>  
-          <li className="pull-right"><a className="fa fa-facebook" style={{color: 'white'}} /></li>
+          <li className="disabled"><h5>Terms of Service</h5></li>
+          <li className="disabled"><h5>Privacy Policy</h5></li>
+          <li className="disabled"><h5>Site Map</h5></li>
+          <li id="copyClk"><h4><strong>clkDoc © 2018</strong></h4></li>
+          <li className="pull-right"><p className="fa fa-github" style={{color: 'white'}} /></li>
+          <li className="pull-right"><p className="fa fa-twitter" style={{color: 'white'}} /></li>  
+          <li className="pull-right"><p className="fa fa-facebook" style={{color: 'white'}} /></li>
         </ul>
       </footer>
 
